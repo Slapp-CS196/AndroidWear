f0cd922d7cf38c859b3fee55178d24a551612a36
unknown <shan.tulshi@gmail.com>
2015-12-07 10:20:42 -0600

    Made official UI layer, untested. Left old UI elements in with visibility "gone", if they need to be brought back.

--- app/src/main/java/cs196slapp/slapp/MainActivity.java
+++ app/src/main/java/cs196slapp/slapp/MainActivity.java
@@ -6,22 +6,33 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
 
-public class MainActivity extends WearableActivity implements SensorEventListener {
+public class MainActivity extends WearableActivity implements SensorEventListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    public static final String TIME_KEY = "xyz.slapp.slapp_android";
 
     private static final SimpleDateFormat AMBIENT_DATE_FORMAT =
             new SimpleDateFormat("HH:mm", Locale.US);
@@ -32,14 +43,15 @@ public class MainActivity extends WearableActivity implements SensorEventListene
     private SensorManager sensorManager;
     private Sensor linear_acc;
 
-    private ArrayList<Float> logx = new ArrayList<Float>();
+/*  private ArrayList<Float> logx = new ArrayList<Float>();         //Leftovers for training implementation
     private ArrayList<Float> logy = new ArrayList<Float>();
     private ArrayList<Float> logz = new ArrayList<Float>();
-
+*/
     private float maxx = (float) 0.0, maxy = (float) 0.0, maxz = (float) 0.0;
     private int slapps = 0;
     private boolean slappActive = false, detection = false, training = false;
 
+    private GoogleApiClient mGoogleApiClient;
 
     private BoxInsetLayout mContainerView;
     private TextView mClockView;
@@ -69,9 +81,16 @@ public class MainActivity extends WearableActivity implements SensorEventListene
         linear_acc = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         sensorManager.registerListener(this, linear_acc, SensorManager.SENSOR_DELAY_NORMAL);
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApiIfAvailable(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
         Button reset_button = (Button) findViewById(R.id.reset_button);
         Button slapp_toggle = (Button) findViewById(R.id.slapp_toggle);
         Button train_toggle = (Button) findViewById(R.id.training_toggle);
+        ImageView logo = (ImageView) findViewById(R.id.logo);
 
         reset_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,6 +109,7 @@ public class MainActivity extends WearableActivity implements SensorEventListene
             public void onClick(View v) {
                 detection = !detection;
                 ((Button)findViewById(R.id.slapp_toggle)).setText("Turn slapping " + ((detection)?"off" : "on"));
+                ((ImageView)findViewById(R.id.logo)).setImageResource(((detection) ? R.drawable.slapp_320_blue : R.drawable.slapp_320_grey));
             }
         });
 
@@ -98,6 +118,25 @@ public class MainActivity extends WearableActivity implements SensorEventListene
             public void onClick(View v) {
                 training = !training;
                 ((Button)findViewById(R.id.training_toggle)).setText("" + ((training)? "Stop" : "Start") + "training");
+            }
+        });
+
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                detection = !detection;
+                ((ImageView)findViewById(R.id.logo)).setImageResource(((detection) ? R.drawable.slapp_320_blue : R.drawable.slapp_320_grey));
+                ((Button)findViewById(R.id.slapp_toggle)).setText("Turn slapping " + ((detection) ? "off" : "on"));
+            }
+        });
+
+        logo.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ((Vibrator)getSystemService(VIBRATOR_SERVICE)).vibrate((long) 50);
+                slapps = 0;
+                maxx = maxy = maxz = (float)0.0;
+                return true;
             }
         });
     }
@@ -117,8 +156,9 @@ public class MainActivity extends WearableActivity implements SensorEventListene
             }else if (event.values[2] <= 9.0 && slappActive) {
                 slappActive = false;
                 Toast.makeText(this, "Slapp!", Toast.LENGTH_SHORT).show();
+                updateDataLayer();
                 try {
-                    Thread.sleep(200);                               //To allow accelerometer values to normalize.
+                    Thread.sleep(500);                               //To allow accelerometer values to normalize.
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -184,7 +224,18 @@ public class MainActivity extends WearableActivity implements SensorEventListene
         } else {
             mContainerView.setBackground(null);
             mClockView.setVisibility(View.GONE);
+            ((TextView)findViewById(R.id.slapp_count)).setText("Slapps: " + slapps);
         }
+    }
+
+    private void updateDataLayer(){
+        PutDataMapRequest putDataMapReq = PutDataMapRequest.create("/time");
+        long time = System.currentTimeMillis();
+        putDataMapReq.getDataMap().putLong(TIME_KEY, time);
+        PutDataRequest putDataReq = putDataMapReq.asPutDataRequest();
+        System.out.println("Data should be updating with this: " + time);
+        PendingResult<DataApi.DataItemResult> pendingResult =
+                Wearable.DataApi.putDataItem(mGoogleApiClient, putDataReq);
     }
 
     public void trainSlapp(){
@@ -194,5 +245,13 @@ public class MainActivity extends WearableActivity implements SensorEventListene
     public boolean checkSlapp() {
         //Just in case Slapp ever gets trained.
         return true;
+    }
+
+    public void onConnectionSuspended (int a) { }
+
+    public void onConnected (Bundle b){ }
+
+    public void onConnectionFailed(ConnectionResult c) {
+        Toast.makeText(this, "Connection Failed!", Toast.LENGTH_SHORT);
     }
 }
